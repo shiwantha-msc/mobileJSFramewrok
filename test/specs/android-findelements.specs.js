98339b1e511455368e566bc040b66d267edc7bb1
ShiwanthaL <shiwantha.lakmal@nagarro.com>
2022-05-19 01:29:39 +0530

    2nd test cases using class locator finder

--- test/specs/android-findelements.specs.js
+++ test/specs/android-findelements.specs.js
@@ -13,4 +13,18 @@ describe('', ()=> {
         await expect(appExpectedEle).toBeExisting();
 
     });
+
+
+    it('', async ()=> {
+
+        //find element by class locator
+        const appTitle = await $('android.view.ViewGroup');
+
+        //print element value
+        console.log(await appTitle.getText());
+
+        //assert value availability
+        expect(appTitle).toHaveText("API DEmos");
+
+    });
 });
